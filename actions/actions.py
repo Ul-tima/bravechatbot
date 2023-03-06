ba84492328825432e74c36bf70ab67a9356ec276
Julia Pavliuk <pavliuk.julia@gmail.com>
2023-03-06 13:27:29 +0200

    Added fallback processing

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,136 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+import json
+
+from typing import Any, Text, Dict, List
+
+from rasa.core.actions.action import create_bot_utterance
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import  SlotSet, EventType
+
+INTENT_DESCRIPTION_MAPPING_PATH = "actions/intent_description_mapping.csv"
+
+
+class ActionDefaultAskAffirmation(Action):
+
+    def name(self) -> Text:
+        return "action_default_ask_affirmation"
+
+    def __init__(self) -> None:
+        import pandas as pd
+
+        self.intent_mappings = pd.read_csv(INTENT_DESCRIPTION_MAPPING_PATH)
+        self.intent_mappings.fillna("", inplace=True)
+        self.intent_mappings.entities = self.intent_mappings.entities.map(
+            lambda entities: {e.strip() for e in entities.split(",")}
+        )
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]
+    ) -> List[EventType]:
+        intent_ranking = tracker.latest_message.get("intent_ranking", [])
+        if len(intent_ranking) > 1:
+            diff_intent_confidence = intent_ranking[0].get(
+                "confidence"
+            ) - intent_ranking[1].get("confidence")
+            if diff_intent_confidence < 0.2:
+                intent_ranking = intent_ranking[:2]
+            else:
+                intent_ranking = intent_ranking[:1]
+
+        # for the intent name used to retrieve the button title, we either use
+        # the name of the name of the "main" intent, or if it's an intent that triggers
+        # the response selector, we use the full retrieval intent name so that we
+        # can distinguish between the different sub intents
+        first_intent_names = [
+            intent.get("name", "")
+            if intent.get("name", "") not in ["out_of_scope", "faq", "chitchat"]
+            else tracker.latest_message.get("response_selector")
+            .get(intent.get("name", ""))
+            .get("full_retrieval_intent")
+            for intent in intent_ranking
+        ]
+
+        message_title = (
+            "🤔 Ви мали на увазі... "
+        )
+
+        entities = tracker.latest_message.get("entities", [])
+        entities = {e["entity"]: e["value"] for e in entities}
+
+        entities_json = json.dumps(entities)
+
+        buttons = []
+        for intent in first_intent_names:
+            button_title = self.get_button_title(intent, entities)
+            if "/" in intent:
+                # here we use the button title as the payload as well, because you
+                # can't force a response selector sub intent, so we need NLU to parse
+                # that correctly
+                buttons.append({"title": button_title, "payload": button_title})
+            else:
+                buttons.append(
+                    {"title": button_title, "payload": f"/{intent}{entities_json}"}
+                )
+
+        buttons.append({"title": "Something else", "payload": "/trigger_rephrase"})
+
+        dispatcher.utter_message(text=message_title, buttons=buttons)
+
+        return []
+
+    def get_button_title(self, intent: Text, entities: Dict[Text, Text]) -> Text:
+        default_utterance_query = self.intent_mappings.intent == intent
+        utterance_query = (self.intent_mappings.entities == entities.keys()) & (
+            default_utterance_query
+        )
+
+        utterances = self.intent_mappings[utterance_query].button.tolist()
+
+        if len(utterances) > 0:
+            button_title = utterances[0]
+        else:
+            utterances = self.intent_mappings[default_utterance_query].button.tolist()
+            button_title = utterances[0] if len(utterances) > 0 else intent
+
+        return button_title.format(**entities)
+
+    #
+    #
+    # class ActionHelloWorld(Action):
+    #
+    #     def name(self) -> Text:
+    #         return "action_hello_world"
+    #
+    #     def run(self, dispatcher: CollectingDispatcher,
+    #             tracker: Tracker,
+    #             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    #
+    #         dispatcher.utter_message(text="Hello World!")
+    #
+    #         return []
+
+    # class ActionTriggerResponseSelector(Action):
+    #     """Returns the chitchat utterance dependent on the intent"""
+    #
+    #     def name(self) -> Text:
+    #         return "action_trigger_response_selector"
+    #
+    #     def run(
+    #             self,
+    #             dispatcher: CollectingDispatcher,
+    #             tracker: Tracker,
+    #             domain: Dict[Text, Any],
+    #     ) -> List[EventType]:
+    #
+    #         retrieval_intent = tracker.get_slot("retrieval_intent")
+    #         if retrieval_intent:
+    #             dispatcher.utter_message(template=f"utter_{retrieval_intent}")
+    #         #return [SlotSet("retrieval_intent", retrieval_intent)]
+    #         return []
+    #
